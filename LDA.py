9eeedc3e581eb9e566b095af462bbb1e60219b28
wrluo <luowenrui_larry@hotmail.com>
2017-05-28 03:37:07 -0700

    final push with PCA VS LDA

--- LDA.py
+++ LDA.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
+from sklearn.decomposition import PCA as sklearnPCA
+
 from matplotlib import pyplot as plt
 import numpy as np
 import math
@@ -74,5 +76,147 @@ y = label_encoder.transform(y) + 1
 
 label_dict = {1: 'Setosa', 2: 'Versicolor', 3:'Virginica'}
 
-showHistograms(X, y, feature_dict, label_dict)
+# showHistograms(X, y, feature_dict, label_dict)
 
+np.set_printoptions(precision=4)
+
+mean_vectors = []
+for cl in range(1,4):
+    mean_vectors.append(np.mean(X[y==cl], axis=0))
+    print('Mean Vector class %s: %s\n' %(cl, mean_vectors[cl-1]))
+
+
+S_W = np.zeros((4,4))
+for cl,mv in zip(range(1,4), mean_vectors):
+    class_sc_mat = np.zeros((4,4))                  # scatter matrix for every class
+    for row in X[y == cl]:
+        row, mv = row.reshape(4,1), mv.reshape(4,1) # make column vectors
+        class_sc_mat += (row-mv).dot((row-mv).T)
+    S_W += class_sc_mat                             # sum class scatter matrices
+print('within-class Scatter Matrix:\n', S_W)
+
+
+overall_mean = np.mean(X, axis=0)
+
+S_B = np.zeros((4,4))
+for i,mean_vec in enumerate(mean_vectors):  
+    n = X[y==i+1,:].shape[0]
+    mean_vec = mean_vec.reshape(4,1) # make column vector
+    overall_mean = overall_mean.reshape(4,1) # make column vector
+    S_B += n * (mean_vec - overall_mean).dot((mean_vec - overall_mean).T)
+
+print('between-class Scatter Matrix:\n', S_B)
+
+
+eig_vals, eig_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+for i in range(len(eig_vals)):
+    eigvec_sc = eig_vecs[:,i].reshape(4,1)   
+    print('\nEigenvector {}: \n{}'.format(i+1, eigvec_sc.real))
+    print('Eigenvalue {:}: {:.2e}'.format(i+1, eig_vals[i].real))
+
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs = sorted(eig_pairs, key=lambda k: k[0], reverse=True)
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+
+print('Eigenvalues in decreasing order:\n')
+for i in eig_pairs:
+    print(i[0])
+
+
+print('Variance explained:\n')
+eigv_sum = sum(eig_vals)
+for i,j in enumerate(eig_pairs):
+    print('eigenvalue {0:}: {1:.2%}'.format(i+1, (j[0]/eigv_sum).real))
+
+
+W = np.hstack((eig_pairs[0][1].reshape(4,1), eig_pairs[1][1].reshape(4,1)))
+print('Matrix W:\n', W.real)
+
+
+X_lda = X.dot(W)
+assert X_lda.shape == (150,2), "The matrix is not 150x2 dimensional."
+
+
+def plot_step_lda():
+
+    ax = plt.subplot(111)
+    for label,marker,color in zip(
+        range(1,4),('^', 's', 'o'),('blue', 'red', 'green')):
+
+        plt.scatter(x=X_lda[:,0].real[y == label],
+                y=X_lda[:,1].real[y == label],
+                marker=marker,
+                color=color,
+                alpha=0.5,
+                label=label_dict[label]
+                )
+
+    plt.xlabel('LD1')
+    plt.ylabel('LD2')
+
+    leg = plt.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.5)
+    plt.title('LDA: Iris projection onto the first 2 linear discriminants')
+
+    # hide axis ticks
+    plt.tick_params(axis="both", which="both", bottom="off", top="off",  
+            labelbottom="on", left="off", right="off", labelleft="on")
+
+    # remove axis spines
+    ax.spines["top"].set_visible(False)  
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)    
+
+    plt.grid()
+    plt.tight_layout
+    plt.show()
+
+sklearn_pca = sklearnPCA(n_components=2)
+X_pca = sklearn_pca.fit_transform(X)
+
+def plot_pca():
+
+    ax = plt.subplot(111)
+
+    for label,marker,color in zip(
+        range(1,4),('^', 's', 'o'),('blue', 'red', 'green')):
+
+        plt.scatter(x=X_pca[:,0][y == label],
+                y=X_pca[:,1][y == label],
+                marker=marker,
+                color=color,
+                alpha=0.5,
+                label=label_dict[label]
+                )
+
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+
+    leg = plt.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.5)
+    plt.title('PCA: Iris projection onto the first 2 principal components')
+
+    # hide axis ticks
+    plt.tick_params(axis="both", which="both", bottom="off", top="off",  
+            labelbottom="on", left="off", right="off", labelleft="on")
+
+    # remove axis spines
+    ax.spines["top"].set_visible(False)  
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)    
+
+    plt.tight_layout
+    plt.grid()
+
+    plt.show()
+
+plot_pca()
+plot_step_lda()
